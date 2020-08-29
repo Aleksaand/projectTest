b9b13838de6cf528e2b93657d64d3570ddc4e271
Aleksandr <ovcharenko_aleksandr@ukr.net>
2020-08-29 17:03:32 +0300

    First Commit

---
+++ JS/js/skript.js
@@ -0,0 +1,10 @@
+"use strict";
+
+console.log(16666);
+let number = 5;
+const leftBorderWidth = 1;
+
+
+number  = 10;
+console.log(number);
+
---
+++ new project/js/script.js
@@ -0,0 +1,32 @@
+// console.log(1);
+
+// let number;
+
+// number = 10;
+// if (number >= 10) {
+//     console.log('true');
+// } else {
+//     console.log('No Bro');
+// }
+// alert('Privet');
+// const result = +prompt("Are you here ?", "kuku");
+
+// console.log(result, typeof(result));
+
+// const answers = [];
+
+// answers[0] = prompt('Как тебя зовут ?', '');
+
+// answers[1] = prompt('Сколько тебе лет ?', '');
+
+// answers[2] = prompt('Ты мужик ?', '');
+
+// answers[99] = prompt('Ты баба ?', '');
+
+// console.log(answers);
+// console.log(typeof(answers));
+// console.log(answers.length);
+
+
+const user = prompt('Kak zovut bro ?', "");
+alert(`Priver ${user}, kak Dela?`);
